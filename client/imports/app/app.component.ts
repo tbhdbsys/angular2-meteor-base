258b3f2e521ca664f560d46b019351e3effab214
Tim Hanson <tim.t.hanson@deutschebahn.com>
2016-11-25 16:36:10 +0100

    Step 3 before we play with mongo. Alas at this point the app dies with 'There is already a collection named "parties" '

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
+import { Parties } from '../../../both/collections/parties.collection';
 import template from './app.component.html';
 
 @Component({
@@ -7,22 +9,9 @@ import template from './app.component.html';
     template
 })
 export class AppComponent {
-    parties: any[];
+    parties: Observable<any[]>;
 
     constructor() {
-        this.parties = [
-            {'name': 'Dubstep-Free Zone',
-                'description': 'Can we please just for an evening not listen to dubstep.',
-                'location': 'Palo Alto'
-            },
-            {'name': 'All dubstep all the time',
-                'description': 'Get it on!',
-                'location': 'Palo Alto'
-            },
-            {'name': 'Savage lounging, whatever that means',
-                'description': 'Leisure suit required. And only fiercest manners.',
-                'location': 'San Francisco'
-            }
-        ];
+        this.parties = Parties.find({}).zone();
     }
 }
